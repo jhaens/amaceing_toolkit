a4da9acd776faabe8b2d017c4ff79b441c1437f3
Jonas Hänseroth <jonas.haenseroth@tu-ilmenau.de>
2025-04-08 09:05:42 +0200

    Python runscript fix for TU Ilmenau & MLU Halle-Wittenberg users

--- src/amaceing_toolkit/default_configs/runscript_templates.py
+++ src/amaceing_toolkit/default_configs/runscript_templates.py
@@ -17,10 +17,10 @@ def hpc_setup_form():
             cluster = 'BatchXL'
             path_to_program['cp2k'] = '/home/joha4087/programs/cp2k_easy/cp2k-2025.1/exe/local/cp2k.popt' 
             path_to_source_file['cp2k'] = '/home/joha4087/programs/cp2k_easy/source_cp2k_gcc_openmpi_by_jonas'
-            path_to_program['mace'] = 'conda activate atk'                                             # to do add the conda env name
+            path_to_program['mace'] = 'conda activate atk'                                                      # to do add the conda env name
             path_to_source_file['mace'] = input('Enter the path to the source file of your conda: [/home/USER/anaconda3/etc/profile.d/conda.sh]')
-            path_to_program['mattersim'] = 'conda activate atk_ms7n'                                           # to do add the conda env name
-            path_to_source_file['mattersim'] = input('Enter the path to the source file of your conda: [/home/USER/anaconda3/etc/profile.d/conda.sh]')
+            path_to_program['mattersim'] = 'conda activate atk_ms7n'                                            # to do add the conda env name
+            path_to_source_file['mattersim'] = path_to_source_file['mace']
             workload_manager = 'lsf'
         elif which_cluster == '2':
             cluster = 'batch6'
@@ -44,7 +44,8 @@ def hpc_setup_form():
         if setup_program == 'y':
             path_to_program['mattersim'] = input('Enter the name of your conda env: [atk_ms7n]')
             path_to_program['mattersim'] = f'conda activate {path_to_program["mattersim"]}'
-            path_to_source_file['mattersim'] = input('Enter the path to the source file of your conda: [/home/USER/anaconda3/etc/profile.d/conda.sh]')
+            path_to_source_file['mattersim'] = path_to_source_file['mace']
+        
 
 
     # Get the path of the current script
